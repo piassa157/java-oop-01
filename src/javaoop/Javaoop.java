b9ab7da44750f7c7e48c6b9441d3c90da84d7bcc
piassa157 <lindaum98@gmail.com>
2021-03-20 13:46:53 -0300

    [GGP] Feat: iNIT

---
+++ src/javaoop/Javaoop.java
@@ -0,0 +1,17 @@
+
+package javaoop;
+
+/**
+ *
+ * @author Piassa
+ */
+public class Javaoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+    }
+    
+}
